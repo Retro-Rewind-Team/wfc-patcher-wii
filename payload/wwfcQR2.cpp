3943fe5cda3432aaa6d5f38807682e7d19404193
ppeb <ppeb@ppeb.me>
2025-04-09 01:53:24 -0500

    Accept multiple PIDs in QR2 kick order

--- payload/wwfcQR2.cpp
+++ payload/wwfcQR2.cpp
@@ -21,9 +21,9 @@ WWFC_DEFINE_PATCH = {
         RMCXD_PORT(0x800d879c, 0x800d86fc, 0x800d86bc, 0x800d87fc), //
         // clang-format off
         [](QR2::qr2_t qrec,
-           char* query,
-           int len,
-           struct sockaddr* sender) -> void {
+            char* query,
+            int len,
+            struct sockaddr* sender) -> void {
 
             LONGCALL void qr2_parse_queryA(
                 QR2::qr2_t qrec, char* query, int len, struct sockaddr* sender
@@ -40,32 +40,40 @@ WWFC_DEFINE_PATCH = {
                 LOG_INFO("QR2: Packet sent by non-master server.");
                 return qr2_parse_queryA(qrec, query, len, sender);
             }
-    
+
             char command = query[2];
             switch (command) {
                 // The server orders us to cut the connection to a PID
-                case PACKET_KICK_ORDER:
-                    if (len >= 12 && DWC::stpMatchCnt) {
-                        u32 pid = *(u32*)&query[8];
+                case PACKET_KICK_ORDER: {
+                    if (len < 12 || !DWC::stpMatchCnt)
+                        break;
+
+                    // Length byte comes after 7 byte qr2 header
+                    u8 pidCount = *(u8*)&query[7];
+
+                    int i = 8;
+                    u8 j = 0;
+                    // Repeated sequence of u32s starting at byte 9 (index 8)
+                    for (; i + 4 < len && j < pidCount; i += 4, j++) {
+                        u32 pid = *(u32*)&query[i];
 
                         // If the PID to remove is the host or ourself, DC from the room
                         if (pid == DWC::stpMatchCnt->serverProfileID || pid == DWC::stpMatchCnt->profileID) {
                             DWC::DWCi_HandleGPError(3);
                             DWC::DWCi_SetError(6, -83337);
                             break;
-                        } 
+                        }
+
                         // If the PID is another peer, close the connection to them
-                        else {                 
-                            DWC::DWCiNodeInfo* nodes = DWC::stpMatchCnt->nodes;
-                            for (int i = 0; i < 32; i++) {
-                                if (nodes[i].profileId == pid) {
-                                    DWC::DWC_CloseConnectionHard(nodes[i].aid);
-                                    break;
-                                }
-                            }
+                        DWC::DWCiNodeInfo* nodes = DWC::stpMatchCnt->nodes;
+                        for (int i = 0; i < 32; i++) {
+                            if (nodes[i].profileId == pid)
+                                DWC::DWC_CloseConnectionHard(nodes[i].aid);
                         }
                     }
+
                     break;
+                }
                 default:
                     break;
             }
